5be1da47b8804e0ca7072fb2c461605ef8ef9e47
Andrei Simedre <simedre.andrei@cylex.ro>
2016-07-14 12:40:22 +0300

    afisare articole autor pe peagina de autor

--- public/autor.php
+++ public/autor.php
@@ -15,13 +15,17 @@ $detaliiAutor = db_select($conn, $query);
 $numeAutor = $detaliiAutor[0]['nume'];
 $email = $detaliiAutor[0]['email'];
 $username = $detaliiAutor[0]['user'];
+if(isset($detaliiAutor[0]['caleImg']) && $detaliiAutor[0]['caleImg'] != '') {
+  $caleImgAutor = $detaliiAutor[0]['caleImg'];
+}
+else {
+  $caleImgAutor = '../images/profile.png';
+}
 
-if(isset($detaliiAutor[0]['caleImg']) && $detaliiAutor[0]['caleImg'] != ''){
-  $caleImg = $detaliiAutor[0]['caleImg'];
-}
-else{
-  $caleImg = '../images/profile.png';
-}
+$query2 = "SELECT * FROM articole WHERE autID =" . "'" . $detaliiAutor[0]['ID'] . "'";
+$detaliiArticol = db_select($conn, $query2);
+
+
 
 echo template('page_tpl', array(
     'page_title' => 'Autor',
@@ -29,6 +33,7 @@ echo template('page_tpl', array(
       'numeAutor' => $numeAutor,
       'username' => $username,
       'email' => $email,
-      'caleImg' => $caleImg
+      'caleImgAutor' => $caleImgAutor,
+      'detaliiArticol' => $detaliiArticol,
       ))
 ));
