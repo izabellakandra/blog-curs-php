87472e4e51408c8ded412f8012ce7eda8d9a0e77
Takacs Hadesz <Takacs Hadesz>
2016-07-17 20:59:22 +0300

    listare dupa data/adaugat buton/afisare data creare

--- templates/main_tpl.php
+++ templates/main_tpl.php
@@ -1,17 +1,25 @@
 <div>
+
+<a href="" class="btn btn-default"> Creare articol </a>
+</div>
+<br>
+
+<div>
+
 <?php
 
 $con = db_connect(array(
         'host' => '127.0.0.1',
         'port' => 3306,
-        'database' => 'cylex',
+        'database' => 'articole',
         'charset' => 'utf8',
         'user' => 'root',
         'pass' => '',
 ));
 
 
-$query = "SELECT * FROM articole";
+
+$query = "SELECT * FROM articole ORDER BY data DESC";
 $results = db_select($con, $query);
 
 echo template('articole/articole_tpl', array(
