12d7da08d820780e31235ad5e9d72a8aa814eeb7
user <tszili@gmail.com>
2016-07-18 11:08:01 +0300

    am adaugat logica in index/scos din tpl

--- public/index.php
+++ public/index.php
@@ -8,11 +8,24 @@ session_start();
 include_once "../inc/functions.php";
 include '../inc/db.php';
 
+$con = db_connect(array(
+        'host' => '127.0.0.1',
+        'port' => 3306,
+        'database' => 'cylex',
+        'charset' => 'utf8',
+        'user' => 'root',
+        'pass' => '',
+));
+$query = "SELECT * FROM articole ORDER BY data DESC";
+$results = db_select($con, $query);
+
+
 echo template('page_tpl', array(
     'page_title' => 'Blog title',
     'banner_tpl' => template('banner_tpl', array(
     	'page_title' => 'GastroCylexia',
     	'page_slogan' => 'Bun venit in lumea retetelor!',
     	)),
-     'content' => template('main_tpl', array())
+     'content' => template('main_tpl', array ( 'art' => template('articole/articole_tpl', array('articole' => $results)
+                                 )))
 ));
--- templates/main_tpl.php
+++ templates/main_tpl.php
@@ -1,36 +1,10 @@
 <div>
-
-<a href="" class="btn btn-default"> Creare articol </a>
+         <a href="" class="btn btn-default"> Creare articol </a>
 </div>
 <br>
 
 <div>
-
-<?php
-
-$con = db_connect(array(
-        'host' => '127.0.0.1',
-        'port' => 3306,
-        'database' => 'cylex',
-        'charset' => 'utf8',
-        'user' => 'root',
-        'pass' => '',
-));
-
-
-
-$query = "SELECT * FROM articole ORDER BY data DESC";
-$results = db_select($con, $query);
-
-echo template('articole/articole_tpl', array(
-    'articole' => $results,
-));
-
-
-?>
-
-
-
-
-
+     <?php
+         echo $art; 
+     ?>
 </div>
