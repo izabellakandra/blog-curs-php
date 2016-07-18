7056ac949161ef47e6cac6abc10c84de008cab59
user <tszili@gmail.com>
2016-07-18 10:55:13 +0300

    adaugare autor

--- templates/articole/articole_tpl.php
+++ templates/articole/articole_tpl.php
@@ -1,13 +1,21 @@
 <div>
-    <?php foreach($articole as $articol): ?>
-    <div>
-        
-            <?php echo html_a($articol['titlu'], '/articol/view.php?id=' . $articol['ID']);  ?>
-            <br><br>
+          <?php foreach($articole as $articol): ?>
+            <h4> 
+                  <?php   echo html_a($articol['titlu'], '/articol/view.php?id=' . $articol['ID']);  ?>
+            </h4>
+            <br>
             <?php echo $articol['continut']; ?>
-			<br><br><p align="right">
-			<?php echo $articol['data']; ?> 
-			</p>
-    </div>
+            <br><br>
+		<div class="row"> 
+                       <em> 
+                       <div class="col-md-offset-1 col-md-4">
+                                      Autor:  <?php echo $articol['autID']; ?> 
+                       </div>
+                       <div class="col-md-offset-3 col-md-4">
+                                       Data: <?php echo $articol['data']; ?> 
+     		       </div>
+                       </em>
+                </div>
+                <br><br>
     <?php endforeach;?>
 </div>
